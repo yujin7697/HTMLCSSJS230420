958ce778a15b94c6493bfd51d0760e4f5bb91a87
jangyujin <juj7697@gmail.com>
2023-07-05 15:39:01 +0900

    0705

---
+++ 12JS_BASIC/JS/main.js
@@ -0,0 +1,6 @@
+console.log("main.js 연결확인");
+const boxEl = document.querySelector('.box');
+//    boxEl.addEventListener('click',function(){})
+boxEl.addEventListener('click',()=>{
+    boxEl.style.backgroundColor="yellow";
+})
